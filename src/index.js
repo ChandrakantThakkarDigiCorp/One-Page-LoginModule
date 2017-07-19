cf064b9bdf760dbddaef44d81e09584c3270de98
chandrakant Thakkar <chandrakant.thakkar@digi-corp.com>
2017-07-19 12:21:02 +0530

    CBT:added files

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Form from "./Form";
+import LoginComponent from "./LoginComponent";
+import registerServiceWorker from './registerServiceWorker';
+
+var element=(<div>
+<link href="http://propeller.in/assets/landing-page/images/favicon.ico" type="image/x-icon" rel="icon" />
+                <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
+                <link href="http://propeller.in/components/card/css/card.css" type="text/css" rel="stylesheet" />
+                <link href="http://propeller.in/docs/css/example-docs.css" type="text/css" rel="stylesheet" />
+                <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+                <link href="http://propeller.in/components/textfield/css/textfield.css" type="text/css" rel="stylesheet" />
+<LoginComponent />
+</div>);
+
+ReactDOM.render(element, document.getElementById('root'));
+registerServiceWorker();
